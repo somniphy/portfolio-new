d26e26f95822b6c4d40d5896e36344f926ad6b13
somniphy <penalescjay@gmail.com>
2025-02-27 02:53:27 +0800

    fix about

--- components/education.tsx
+++ components/education.tsx
@@ -2,22 +2,22 @@ import { TextEncrypted } from "./text-encrypted";
 
 export default function Education() {
   return (
-    <section className="px-[5.5%] relative space-y-6">
+    <section className="relative py-16">
       <h2 className="text-white text-7xl tracking-wide uppercase font-bold will-change-auto">
         <TextEncrypted text="Education" interval={50} />
       </h2>
       <hr className="border-white border-1 w-full" />
       <div className="relative w-full space-y-6">
         <div>
-          <h1 className="text-2xl uppercase font-bold mb-2">
+          <h1 className="text-2xl uppercase font-bold mt-4 text-orange-600">
             2019 - Iligan Computer Institute
           </h1>
           <p className="text-sm uppercase text-white">
             Information and Communication Technology
           </p>
         </div>
-        <div>
-          <h1 className="text-2xl uppercase font-bold mb-2">
+        <div className="">
+          <h1 className="text-2xl uppercase font-bold mt-12 text-orange-600">
             2024 - Mindanao State University - Iligan Institute of Technology
           </h1>
           <p className="text-sm uppercase text-white">
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,16 +1,35 @@
-import AboutSection from "@/components/about";
-
 import Skills from "@/components/skills";
 import Experience from "@/components/experience";
 import Education from "@/components/education";
+import Navlink from "@/components/navlink";
+
+import { TextEncrypted } from "@/components/text-encrypted";
+import TextStagger from "@/components/text-stagger";
 
 export default function About() {
   return (
-    <main className="max-w-screen mx-auto relative min-h-screen flex flex-col gap-16 py-12">
-      <AboutSection />
-      <Skills />
-      <Experience />
-      <Education />
+    <main>
+      <div className="min-h-screen min-w-screen h-screen w-screen">
+        <div className="container mx-auto flex flex-col mt-24 items-left">
+          <TextEncrypted
+            text="About"
+            className="text-6xl md:text-7xl lg:text-8xl font-bold text-white uppercase"
+          />
+          <TextStagger className="text-2xl md:text-3xl lg:text-4xl text-white uppercase font-medium md:mt-4">
+            {/* Description */}Christian Penales is a
+            <span className="text-orange-600 px-2">Web Developer</span>
+            and<span className="text-orange-600 px-2">UI/UX Designer</span>
+            with a knack for creating intuitive, user-centered digital
+            experiences. With a strong foundation in both design and
+            development, I bridge the gap between{" "}
+            <span className="italic px-2">aesthetics</span>&
+            <span className="font-bold px-2"> functionality.</span>
+          </TextStagger>
+          <Skills />
+          <Experience />
+          <Education />
+        </div>
+      </div>
     </main>
   );
 }
